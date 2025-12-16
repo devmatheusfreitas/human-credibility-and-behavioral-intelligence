ff5fd12877dd0a385605c0844d97ef8c8113266e
Matheus Freitas <matheusdefreitas.tech@gmail.com>
2025-12-16 19:11:47 -0300

    This is the mapping file that identifies which part of the face each group of points belongs to.

---
+++ landmark_map.py
@@ -0,0 +1,35 @@
+FACIAL_LANDMARKS = {
+    "eyes": {
+        "left": {
+            "outer": 33,
+            "inner": 133,
+            "top": 159,
+            "bottom": 145
+        },
+        "right": {
+            "outer": 362,
+            "inner": 263,
+            "top": 386,
+            "bottom": 374
+        }
+    },
+
+    "eyebrows": {
+        "left": [65, 105, 70, 63],
+        "right": [295, 334, 300, 293]
+    },
+
+    "mouth": {
+        "upper_lip": 13,
+        "lower_lip": 14,
+        "corners": {
+            "left": 78,
+            "right": 308
+        }
+    },
+
+    "nose": {
+        "tip": 1,
+        "bridge": [168, 197]
+    }
+}
