a2d3bb0b9bc52240915f2ecb5f3e62ed5f19fd9d
Matheus Freitas <matheusdefreitas.tech@gmail.com>
2025-12-16 18:56:04 -0300

    main.py updated with face mesh fully implemented and saving all points into the landmarks.py file

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import cv2
 import mediapipe as mp
+import csv
+import time
 
 
+# Creating an CSV file ["frame,timestamp,landmark_id,x,y,z"]
+csv_file = open("face_landmarks.csv", "w", newline="")
+csv_writer = csv.writer(csv_file)
+
+# CSV Pattern
+csv_writer.writerow([
+    "frame",
+    "timestamp",
+    "landmark_id",
+    "x",
+    "y",
+    "z"
+])
+
+# Face Mesh Inicialization
 mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+mp_drawing_styles = mp.solutions.drawing_styles        # Styles (points, weight, size)
 
+
+frame_count = 0
+start_time = time.time()
 
 cap = cv2.VideoCapture(0)                                # Start capturing -> 0, 1, 2, etc.
-
 
 with mp_face_mesh.FaceMesh(
     static_image_mode=False,
@@ -17,19 +36,28 @@ with mp_face_mesh.FaceMesh(
     min_detection_confidence=0.6,
     min_tracking_confidence=0.6
 ) as face_mesh:
-
     
     #Main Loop:
-
     while cap.isOpened():
-        ret, frame = cap.read()                          # If ret=true, go!
-        if not ret:
+        ret, frame = cap.read()                          
+        if not ret:                                     # If ret=true, go!
             break
-
+        
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = face_mesh.process(rgb_frame)
 
         for face_landmarks in results.multi_face_landmarks:
+            for idx, landmark in enumerate(face_landmarks.landmark):
+                csv_writer.writerow([
+                    frame_count,
+                    time.time() - start_time,
+                    idx,
+                    landmark.x,
+                    landmark.y,
+                    landmark.z
+                ])
+            frame_count += 1
+
             mp_drawing.draw_landmarks(
                 image=frame,
                 landmark_list=face_landmarks,
@@ -43,6 +71,7 @@ with mp_face_mesh.FaceMesh(
 
         if cv2.waitKey(1) & 0xFF == 27:                     # WaitKey(1) refreshes 1ms and wait for detected 'esc' key pressed
             break
-
+        
+csv_file.close()
 cap.release()                                               # Turn off webcam
 cv2.destroyAllWindows()                                     # Close window and clean all
